2de6c25f8da23b53b6a15591cfa5dd00ced8bf49
Josh Kramer <josh.kramer@measuresforjustice.org>
2020-03-17 00:25:56 -0400

    Init database reading

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,10 @@
 console.log('foo')
+
+export const start = () => {
+  const db = firebase.firestore()
+  db.collection('races').get().then(querySnapshot => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, doc.data())
+    })
+  })
+}
