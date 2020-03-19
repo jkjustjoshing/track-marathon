08931bcd21a38637bb1740f067f2a7d7387765cc
Josh Kramer <josh.kramer@measuresforjustice.org>
2020-03-18 23:24:28 -0400

    Hook up table to data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-export const start = () => {
-
-
-  const unsubscribe = listenForData(data => {
-    console.log(data)
-  })
+const useRaceData = () => {
+  const race = useMemo(() => {
+    const db = window.firebase.firestore()
+    return db.collection('races').doc('VegflgL0UgoDqBOOckN7')
+  }, [])
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    return race.onSnapshot(doc => {
+      if (doc.exists) {
+        setData(doc.data())
+      }
+    })
+  }, [race])
+  return data
 }
 
+const secondsToTime = (time) => {
+  const minutes = Math.floor(time / 60)
+  const seconds = Math.round(time - (minutes * 60))
 
-const listenForData = (cb) => {
-  const db = window.firebase.firestore()
-  const race = db.collection('races').doc('VegflgL0UgoDqBOOckN7')
-
-  return race.onSnapshot((doc) => {
-    if (doc.exists) {
-      cb(doc.data())
-    }
-  })
+  return minutes + ':' + String(seconds).padStart(2, '0')
 }
-
 
 const App = () => {
 
-  useEffect(() => {
-    start()
-  }, [])
 
-  return (
+  const data = useRaceData()
+
+  console.log(data)
+
+  return data && (
     <table id='lap-table'>
-      <tr>
-        <th>Lap</th>
-        <th>Time</th>
-        <th>Pace</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Lap</th>
+          <th>Time</th>
+          <th>Pace</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.laps.map((lap, id) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{secondsToTime(lap.duration)}</td>
+              <td>{secondsToTime((1600 / lap.distance) * lap.duration)} per mile</td>
+              <td>{JSON.stringify(lap)}</td>
+            </tr>
+          ))
+        }
+      </tbody>
     </table>
   );
 }
