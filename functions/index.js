a4194a710efac1547c1c41f9cdcdf4cccce55486
Josh Kramer <josh.kramer@measuresforjustice.org>
2020-03-26 22:49:11 -0400

    Add CORS to getServerTime function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions');
 
+const whitelist = ['https://track-marathon.kramer.run', 'http://localhost:3000']
+
 exports.getServerTime = functions.https.onRequest(async (req, res) => {
+  const origin = req.headers.origin
+  if (whitelist.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.json({ time: (new Date().getTime()) });
 });
