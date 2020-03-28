d0ebd8c299c6e6eca91db72d021c1bd00a8378dd
Josh Kramer <josh.kramer@measuresforjustice.org>
2020-03-27 21:18:34 -0400

    Confirm button

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
+import ReactDOM from 'react-dom'
 import { usePushData } from '../../useRaceData'
 import useAuth from '../../useAuth'
 import './Admin.scss'
@@ -13,12 +14,11 @@ const Admin = ({ raceId, data, children }) => {
 
   return (
     <div className='admin'>
-      <div className='admin__content-wrapper'>{children}</div>
+      <ConfirmButton onClick={logout}>logout</ConfirmButton>
       <div className='admin__wrapper'>
         <select value={data.currentLane} onChange={e => setLane(e.target.value)}>
           {Array(6).fill(null).map((_, i) => <option key={i} value={i+1}>Lane {i+1}</option>)}
         </select>
-        <button onClick={logout}>logout</button>
         <button onClick={start}>Start</button>
         <button onClick={() => { addLap(data.currentLane) }}>Trigger lap</button>
       </div>
@@ -42,5 +42,42 @@ const Login = () => {
       <input type='password' placeholder='password' onChange={e => setPassword(e.target.value)} value={password} />
       <button type='submit'>Login</button>
     </form>
+  )
+}
+
+const ConfirmButton = ({ onClick, ...props }) => {
+
+  const [state, dispatch] = useReducer((state, { action, event }) => {
+    if (action === 'click') {
+      event.preventDefault()
+      return { state: 'show_overlay', event: event }
+    } else if (action === 'confirm') {
+      onClick && onClick(state.event)
+      return { state: 'base'}
+    } else if (action === 'cancel') {
+      return { state: 'base' }
+    }
+  }, { state: 'base' })
+
+  const initClick = (event) => {
+    event.persist()
+    dispatch({ action: 'click', event })
+  }
+
+  return (
+    <>
+      <button onClick={initClick} {...props} />
+      {
+        state.state === 'show_overlay' && ReactDOM.createPortal(
+          (
+            <div className='popup'>
+              <button onClick={() => dispatch({ action: 'confirm' })} style={{ background: 'green' }}>Confirm</button>
+              <button onClick={() => dispatch({ action: 'cancel' })} style={{ background: 'red' }}>Cancel</button>
+            </div>
+          ),
+          document.getElementById('popup')
+        )
+      }
+    </>
   )
 }
