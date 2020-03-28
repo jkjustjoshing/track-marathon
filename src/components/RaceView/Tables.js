9b654f928d00f2c17f604a4fc16284c47a5369de
Josh Kramer <josh.kramer@measuresforjustice.org>
2020-03-27 22:04:38 -0400

    Add props to show reduced data in table

--- src/components/RaceView/Tables.js
+++ src/components/RaceView/Tables.js
@@ -3,14 +3,14 @@ import ElapsedTime from '../DataFields/ElapsedTime'
 import { pace, duration, metersToMiles, milesToMeters, laneToDistance, firebaseDate } from '../../utils'
 import './Table.scss'
 
-export const LapsTable = ({ data, reverse = false }) => {
+export const LapsTable = ({ data, reverse = false, limit, hideLane = false }) => {
 
   const currentRow = (
     <tr className='table__current-row'>
       <td>{data.laps.length + 1}</td>
       <td><ElapsedTime time={data.laps[data.laps.length - 1]?.end || data.start} decimals={1} /></td>
       <td> - </td>
-      <td>{data.currentLane} ({laneToDistance(data.currentLane)}m)</td>
+      {hideLane ? null : <td>{data.currentLane} ({laneToDistance(data.currentLane)}m)</td>}
       <td> - </td>
       <td> - </td>
     </tr>
@@ -31,7 +31,7 @@ export const LapsTable = ({ data, reverse = false }) => {
           <th>Lap</th>
           <th>Time</th>
           <th>Pace</th>
-          <th>Lane (distance in meters)</th>
+          {hideLane ? null : <th>Lane (distance in meters)</th>}
           <th>Total Time</th>
           <th>Total Distance</th>
         </tr>
@@ -39,7 +39,7 @@ export const LapsTable = ({ data, reverse = false }) => {
       <tbody>
         { !reverse && currentRow }
         {
-          (reverse ? laps : [...laps].reverse()).map((lap, index) => {
+          (reverse ? laps : [...laps].reverse()).filter((_, i) => limit ? i < limit : true).map((lap, index) => {
             const duration = lap.end.seconds - lap.start.seconds
             const totalDuration = lap.end.seconds - laps[0].start.seconds
             const lapId = reverse ? index + 1 : data.laps.length - index
@@ -51,9 +51,11 @@ export const LapsTable = ({ data, reverse = false }) => {
                 <td>
                   <ElapsedTime duration={pace({ distance: lap.distance, duration })} /> / mile
                 </td>
-                <td>
-                  {lap.laneNumber} ({lap.distance}m)
-                </td>
+                {hideLane ? null : (
+                  <td>
+                    {lap.laneNumber} ({lap.distance}m)
+                  </td>
+                )}
                 <td><ElapsedTime duration={totalDuration} /></td>
                 <td>{metersToMiles(lap.totalDistance).toFixed(2)}mi</td>
               </tr>
